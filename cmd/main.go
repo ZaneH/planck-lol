8b73f7798078e346a041291b55be8175467d2526
Zane Helton <zane99@me.com>
2025-07-07 18:01:35 -0400

    Extract main into func run()

--- cmd/main.go
+++ cmd/main.go
@@ -9,20 +9,26 @@ import (
 )
 
 func main() {
-	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	err := run()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Unhandled error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func run() error {
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return err
+	}
+
 	defer dbpool.Close()
 
-	storage := &shortener.LinkStorage{
-		DBPool: dbpool,
-	}
-	linkService := &shortener.LinkService{
-		Storage: storage,
-	}
-
+	storage := &shortener.LinkStorage{DBPool: dbpool}
+	linkService := &shortener.LinkService{Storage: storage}
 	server := shortener.SetupServer(linkService)
+
+	fmt.Printf("Listening on %s\n", server.Addr)
 	server.ListenAndServe()
+	return nil
 }
